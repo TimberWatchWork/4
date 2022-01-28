5c1bf30e187141812d8333e6df72f7f870582080
TW <timberwatch.work@gmail.com>
2022-01-29 01:35:51 +0500

    Add "Сondorcet's model"

--- 4/systems.py
+++ 4/systems.py
@@ -1,35 +1,66 @@
 import tkinter as tk
 
-def click1(UI):
-    # res = "Привет {}".format(UI.txt1.get())
-    a = int(UI.txt1.get()) + int(UI.txt2.get())
-    b = int(UI.txt3.get()) + int(UI.txt4.get())
-    c = int(UI.txt5.get()) + int(UI.txt6.get())
-    m, i, winner = a, 0, ""
-    if b > m:
-        m = b
-    if c > m:
-        m = c
-    print('m= ', m)
-    if m == a:
-        winner = "Победил кандидат A"
-        i += 1
-    if m == b:
-        winner = "Победил кандидат B"
-        i += 1
-    if m == c:
-        winner = "Победил кандидат C"
-        i += 1
-    print('i= ', i)
-    if i > 1:
-        winner = "Ничья, победителя нет"
-    print('winner= ', winner)
 
-    UI.txt.configure(state='normal')
-    UI.txt.delete("1.0", "end")
-    UI.txt.insert(tk.INSERT, f'Подсчёт голосов: \n'
-                             f"за кандидата А: {a} голосов \n"
-                             f"за кандидата B: {b} голосов \n"
-                             f"за кандидата C: {c} голосов \n"
-                             f"{winner} \n")
-    UI.txt.configure(state='disabled')
+def click1(UI):
+    method = UI.combo.get()[0]
+    winner = ""
+    # Метод относительного большинства
+    if method == "1":
+        # Подсчитываем голоса
+        a = int(UI.txt1.get()) + int(UI.txt2.get())
+        b = int(UI.txt3.get()) + int(UI.txt4.get())
+        c = int(UI.txt5.get()) + int(UI.txt6.get())
+        # Определяем победителя
+        m, i = a, 0
+        if b > m: m = b
+        if c > m: m = c
+        if m == a: winner = "Победил кандидат A"; i += 1
+        if m == b: winner = "Победил кандидат B"; i += 1
+        if m == c: winner = "Победил кандидат C"; i += 1
+        if i > 1: winner = "Ничья, победителя нет"
+        # Выводим результаты
+        UI.txt.configure(state='normal')
+        UI.txt.delete("1.0", "end")
+        UI.txt.insert(tk.INSERT, f'Подсчёт голосов: \n'
+                                 f"за кандидата А: {a} голосов \n"
+                                 f"за кандидата B: {b} голосов \n"
+                                 f"за кандидата C: {c} голосов \n"
+                                 f"{winner} \n")
+        UI.txt.configure(state='disabled')
+
+    # Метод Кондорсе
+    if method == "2":
+        # Подсчет победителей в каждой паре
+        ab = int(UI.txt1.get()) + int(UI.txt2.get()) + int(UI.txt5.get())
+        ba = int(UI.txt3.get()) + int(UI.txt4.get()) + int(UI.txt5.get())
+        ac = int(UI.txt1.get()) + int(UI.txt2.get()) + int(UI.txt3.get())
+        ca = int(UI.txt4.get()) + int(UI.txt5.get()) + int(UI.txt6.get())
+        bc = int(UI.txt1.get()) + int(UI.txt3.get()) + int(UI.txt4.get())
+        cb = int(UI.txt2.get()) + int(UI.txt5.get()) + int(UI.txt6.get())
+
+        if ab > ba: zab = "A "+str(ab)+" > B "+str(ba)
+        elif ab == ba: zab = "A "+str(ab)+" = B "+str(ba)
+        else: zab = "B "+str(ba)+" > A "+str(ab)
+
+        if bc > cb: zbc = "B "+str(bc)+" > C "+str(cb)
+        elif bc == cb: zbc = "B "+str(bc)+" = C "+str(cb)
+        else: zbc = "C "+str(cb)+" > B "+str(bc)
+
+        if ac > ca: zac = "A "+str(ac)+" > C "+str(ca)
+        elif ac == ca: zac = "A "+str(ac)+" = C "+str(ca)
+        else: zac = "C "+str(ca)+" > A "+str(ac)
+
+        if (ab > ba) & (ac > ca): winner = "Победил кандидат A"
+        elif (ba > ab) & (bc > cb): winner = "Победил кандидат B"
+        elif (cb > bc) & (ca > ac): winner = "Победил кандидат C"
+        else: winner = "Ничья, победителя нет"
+        # Выводим результаты
+        UI.txt.configure(state='normal')
+        UI.txt.delete("1.0", "end")
+        UI.txt.insert(tk.INSERT, f'Попарное сравнение: \n'
+                                 f"{zab} \n"
+                                 f"{zbc} \n"
+                                 f"{zac} \n"
+                                 f"{winner} \n")
+        UI.txt.configure(state='disabled')
+
--- 4/window.py
+++ 4/window.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from tkinter import ttk, scrolledtext
+from tkinter import scrolledtext
+from tkinter.ttk import Combobox
 from systems import *
 
 
@@ -11,19 +12,6 @@ class MakeUI(Frame):
         Frame.__init__(self, width=500, height=250, *args, **kwargs)
         self.pack(expand=1, fill='both')
 
-        # Вкладки
-        self.tab_control = ttk.Notebook(self)
-        self.tab1 = ttk.Frame(self.tab_control)
-        self.tab2 = ttk.Frame(self.tab_control)
-        self.tab3 = ttk.Frame(self.tab_control)
-        self.tab4 = ttk.Frame(self.tab_control)
-        self.tab5 = ttk.Frame(self.tab_control)
-        self.tab_control.add(self.tab1, text='1 Относительное большинство')
-        self.tab_control.add(self.tab2, text='2.1 Кондорсе')
-        self.tab_control.add(self.tab3, text='2.2 Копленд')
-        self.tab_control.add(self.tab4, text='2.3 Симпсон')
-        self.tab_control.add(self.tab5, text='3 Борд')
-
         # Относительное большинство
         var1 = IntVar()
         var2 = IntVar()
@@ -32,43 +20,47 @@ class MakeUI(Frame):
         var5 = IntVar()
         var6 = IntVar()
         var1.set(0), var2.set(0), var3.set(0), var4.set(0), var5.set(0), var6.set(0)
-        self.lbl1 = Label(self.tab1, text="A > B > C", padx=10, pady=5, width=10)
-        self.lbl1.grid(column=0, row=0)
-        self.txt1 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var1)
-        self.txt1.grid(column=1, row=0, padx=5)
-        self.lbl2 = Label(self.tab1, text="A > C > B", padx=10, pady=5, width=10)
-        self.lbl2.grid(column=0, row=1)
-        self.txt2 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var2)
-        self.txt2.grid(column=1, row=1, padx=5)
-        self.lbl3 = Label(self.tab1, text="B > A > C", padx=10, pady=5, width=10)
-        self.lbl3.grid(column=0, row=2)
-        self.txt3 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var3)
-        self.txt3.grid(column=1, row=2, padx=5)
-        self.lbl4 = Label(self.tab1, text="B > C > A", padx=10, pady=5, width=10)
-        self.lbl4.grid(column=0, row=3)
-        self.txt4 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var4)
-        self.txt4.grid(column=1, row=3, padx=5)
-        self.lbl5 = Label(self.tab1, text="C > A > B", padx=10, pady=5, width=10)
-        self.lbl5.grid(column=0, row=4)
-        self.txt5 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var5)
-        self.txt5.grid(column=1, row=4, padx=5)
-        self.lbl6 = Label(self.tab1, text="C > B > A", padx=10, pady=5, width=10)
-        self.lbl6.grid(column=0, row=5)
-        self.txt6 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var6)
-        self.txt6.grid(column=1, row=5, padx=5)
+        self.lbl = Label(self, text=" Выберите модель: ", padx=10, pady=5, width=15)
+        self.lbl.grid(column=0, row=0, columnspan=2)
+        self.lbl1 = Label(self, text="A > B > C", padx=10, pady=5, width=10)
+        self.lbl1.grid(column=0, row=1)
+        self.txt1 = Spinbox(self, width=5, from_=0, to=1000, textvariable=var1)
+        self.txt1.grid(column=1, row=1, padx=5)
+        self.lbl2 = Label(self, text="A > C > B", padx=10, pady=5, width=10)
+        self.lbl2.grid(column=0, row=2)
+        self.txt2 = Spinbox(self, width=5, from_=0, to=1000, textvariable=var2)
+        self.txt2.grid(column=1, row=2, padx=5)
+        self.lbl3 = Label(self, text="B > A > C", padx=10, pady=5, width=10)
+        self.lbl3.grid(column=0, row=3)
+        self.txt3 = Spinbox(self, width=5, from_=0, to=1000, textvariable=var3)
+        self.txt3.grid(column=1, row=3, padx=5)
+        self.lbl4 = Label(self, text="B > C > A", padx=10, pady=5, width=10)
+        self.lbl4.grid(column=0, row=4)
+        self.txt4 = Spinbox(self, width=5, from_=0, to=1000, textvariable=var4)
+        self.txt4.grid(column=1, row=4, padx=5)
+        self.lbl5 = Label(self, text="C > A > B", padx=10, pady=5, width=10)
+        self.lbl5.grid(column=0, row=5)
+        self.txt5 = Spinbox(self, width=5, from_=0, to=1000, textvariable=var5)
+        self.txt5.grid(column=1, row=5, padx=5)
+        self.lbl6 = Label(self, text="C > B > A", padx=10, pady=5, width=10)
+        self.lbl6.grid(column=0, row=6)
+        self.txt6 = Spinbox(self, width=5, from_=0, to=1000, textvariable=var6)
+        self.txt6.grid(column=1, row=6, padx=5)
 
-        # self.combo = Combobox(self.tab1)
-        # self.combo['values'] = (1, 2, 3, 4, 5)
-        # self.combo.current(0)  # установите вариант по умолчанию
-        # self.combo.grid(column=0, row=6)
+        self.combo = Combobox(self, width=40)
+        self.combo['values'] = ("1 Относительного большинства",
+                                "2 Модель Кондорсе: явный победитель",
+                                "3 Модель Кондорсе: Правило Копленда",
+                                "4 Модель Кондорсе: Правило Симпсона",
+                                "5 Модель Борда")
+        self.combo.current(0)  # установите вариант по умолчанию
+        self.combo.grid(column=2, row=0)
 
-        self.btn = Button(self.tab1, text="Подсчёт", command=self.clicked)
-        self.btn.grid(column=2, row=6, padx=5)
+        self.btn = Button(self, text="Подсчёт", command=self.clicked)
+        self.btn.grid(column=2, row=7, padx=5)
 
-        self.txt = scrolledtext.ScrolledText(self.tab1, width=40, height=10, state='disabled')
-        self.txt.grid(column=2, row=0, rowspan=6)
-
-        self.tab_control.pack(expand=1, fill='both')
+        self.txt = scrolledtext.ScrolledText(self, width=40, height=10, state='disabled')
+        self.txt.grid(column=2, row=1, rowspan=6)
 
     def clicked(self):
         click1(self)
@@ -77,7 +69,7 @@ class MakeUI(Frame):
 def main():
     window = Tk()
     MakeUI()
-    window.title("Коллективные решения")
+    window.title("Модели принятия коллективных решений")
     window.geometry('500x250')
     window.mainloop()
 
