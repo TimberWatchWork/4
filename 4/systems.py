a42d6c91952504b32a5a9445f530194eb5e3a2e6
TW <timberwatch.work@gmail.com>
2022-01-28 23:49:00 +0500

    Add "relative majority"

--- 4/systems.py
+++ 4/systems.py
@@ -1,5 +1,35 @@
-from tkinter import *
+import tkinter as tk
 
-def clicked(UI):
-    res = "Привет {}".format(UI.txt.get())
-    UI.lbl.configure(text=res)
+def click1(UI):
+    # res = "Привет {}".format(UI.txt1.get())
+    a = int(UI.txt1.get()) + int(UI.txt2.get())
+    b = int(UI.txt3.get()) + int(UI.txt4.get())
+    c = int(UI.txt5.get()) + int(UI.txt6.get())
+    m, i, winner = a, 0, ""
+    if b > m:
+        m = b
+    if c > m:
+        m = c
+    print('m= ', m)
+    if m == a:
+        winner = "Победил кандидат A"
+        i += 1
+    if m == b:
+        winner = "Победил кандидат B"
+        i += 1
+    if m == c:
+        winner = "Победил кандидат C"
+        i += 1
+    print('i= ', i)
+    if i > 1:
+        winner = "Ничья, победителя нет"
+    print('winner= ', winner)
+
+    UI.txt.configure(state='normal')
+    UI.txt.delete("1.0", "end")
+    UI.txt.insert(tk.INSERT, f'Подсчёт голосов: \n'
+                             f"за кандидата А: {a} голосов \n"
+                             f"за кандидата B: {b} голосов \n"
+                             f"за кандидата C: {c} голосов \n"
+                             f"{winner} \n")
+    UI.txt.configure(state='disabled')
--- 4/window.py
+++ 4/window.py
@@ -1,39 +1,86 @@
 from tkinter import *
-from tkinter import ttk
-
+from tkinter import ttk, scrolledtext
 from systems import *
 
 
+class MakeUI(Frame):
 
-# Окно
-window = Tk()
-window.title("Коллективные решения")
-window.geometry('500x250')
-# Вкладки
-tab_control = ttk.Notebook(window)
-tab1 = ttk.Frame(tab_control)
-tab2 = ttk.Frame(tab_control)
-tab3 = ttk.Frame(tab_control)
-tab4 = ttk.Frame(tab_control)
-tab5 = ttk.Frame(tab_control)
-tab_control.add(tab1, text='1 Относительное большинство')
-tab_control.add(tab2, text='2.1 Кондорсе')
-tab_control.add(tab3, text='2.2 Копленд')
-tab_control.add(tab4, text='2.3 Симпсон')
-tab_control.add(tab5, text='3 Борд')
+    def __init__(self, *args, **kwargs):
 
-# Относительное большинство
-lbl = Label(tab1, text="Привет", padx=10, pady=5, width=50)
-lbl.grid(column=0, row=0)
-txt = Entry(tab1, width=5)
-txt.grid(column=1, row=0, padx=5)
-btn = Button(tab1, text="Клик!", command=clicked)
-btn.grid(column=2, row=1, padx=5)
+        # Heritage
+        Frame.__init__(self, width=500, height=250, *args, **kwargs)
+        self.pack(expand=1, fill='both')
 
-# Кондорсе
-# Копленд
-# Симпсон
-# Борд
+        # Вкладки
+        self.tab_control = ttk.Notebook(self)
+        self.tab1 = ttk.Frame(self.tab_control)
+        self.tab2 = ttk.Frame(self.tab_control)
+        self.tab3 = ttk.Frame(self.tab_control)
+        self.tab4 = ttk.Frame(self.tab_control)
+        self.tab5 = ttk.Frame(self.tab_control)
+        self.tab_control.add(self.tab1, text='1 Относительное большинство')
+        self.tab_control.add(self.tab2, text='2.1 Кондорсе')
+        self.tab_control.add(self.tab3, text='2.2 Копленд')
+        self.tab_control.add(self.tab4, text='2.3 Симпсон')
+        self.tab_control.add(self.tab5, text='3 Борд')
 
-tab_control.pack(expand=1, fill='both')
-window.mainloop()
+        # Относительное большинство
+        var1 = IntVar()
+        var2 = IntVar()
+        var3 = IntVar()
+        var4 = IntVar()
+        var5 = IntVar()
+        var6 = IntVar()
+        var1.set(0), var2.set(0), var3.set(0), var4.set(0), var5.set(0), var6.set(0)
+        self.lbl1 = Label(self.tab1, text="A > B > C", padx=10, pady=5, width=10)
+        self.lbl1.grid(column=0, row=0)
+        self.txt1 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var1)
+        self.txt1.grid(column=1, row=0, padx=5)
+        self.lbl2 = Label(self.tab1, text="A > C > B", padx=10, pady=5, width=10)
+        self.lbl2.grid(column=0, row=1)
+        self.txt2 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var2)
+        self.txt2.grid(column=1, row=1, padx=5)
+        self.lbl3 = Label(self.tab1, text="B > A > C", padx=10, pady=5, width=10)
+        self.lbl3.grid(column=0, row=2)
+        self.txt3 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var3)
+        self.txt3.grid(column=1, row=2, padx=5)
+        self.lbl4 = Label(self.tab1, text="B > C > A", padx=10, pady=5, width=10)
+        self.lbl4.grid(column=0, row=3)
+        self.txt4 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var4)
+        self.txt4.grid(column=1, row=3, padx=5)
+        self.lbl5 = Label(self.tab1, text="C > A > B", padx=10, pady=5, width=10)
+        self.lbl5.grid(column=0, row=4)
+        self.txt5 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var5)
+        self.txt5.grid(column=1, row=4, padx=5)
+        self.lbl6 = Label(self.tab1, text="C > B > A", padx=10, pady=5, width=10)
+        self.lbl6.grid(column=0, row=5)
+        self.txt6 = Spinbox(self.tab1, width=5, from_=0, to=1000, textvariable=var6)
+        self.txt6.grid(column=1, row=5, padx=5)
+
+        # self.combo = Combobox(self.tab1)
+        # self.combo['values'] = (1, 2, 3, 4, 5)
+        # self.combo.current(0)  # установите вариант по умолчанию
+        # self.combo.grid(column=0, row=6)
+
+        self.btn = Button(self.tab1, text="Подсчёт", command=self.clicked)
+        self.btn.grid(column=2, row=6, padx=5)
+
+        self.txt = scrolledtext.ScrolledText(self.tab1, width=40, height=10, state='disabled')
+        self.txt.grid(column=2, row=0, rowspan=6)
+
+        self.tab_control.pack(expand=1, fill='both')
+
+    def clicked(self):
+        click1(self)
+
+
+def main():
+    window = Tk()
+    MakeUI()
+    window.title("Коллективные решения")
+    window.geometry('500x250')
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
